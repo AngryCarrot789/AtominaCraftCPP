32e7aa0f59397b7c7f07606ce13c38e32d1af207
jrd <kettlesimzulator420@gmail.com>
2020-11-22 21:36:29 +0000

    Fixed the AABB debug drawing

--- AtominaCraft/Game/Debugging/DebugDraw.h
+++ AtominaCraft/Game/Debugging/DebugDraw.h
@@ -5,12 +5,8 @@
 #include "../../Math/Matrix4.h"
 #include "../../Rendering/Camera.h"
 
-//void DrawVertex(Vector4 v) {
-//	glVertex4f(v.x, v.y, v.z, v.w);
-//}
-
-void DrawVertex(Vector3 v) {
-	glVertex3f(v.x, v.y, v.z);
+void DrawVertex(Vector4 v) {
+	glVertex4f(v.x, v.y, v.z, v.w);
 }
 
 void DrawAABB(const Camera& cam, AxisAlignedBB aabb) {
@@ -18,34 +14,22 @@ void DrawAABB(const Camera& cam, AxisAlignedBB aabb) {
 	glUseProgram(0);
 	glColor3f(0.0f, 1.0f, 0.0f);
 
-	//Point pos = aabb.GetCenter();
-	//Size size = aabb.GetSize();
-	//const Matrix4 mvp = cam.Matrix() * Matrix4::CreateLocalToWorld(pos, Euler(0, 0, 0), size);
-	//
-	//Vector4 v;
-	//Vector3 vp;
-	//
-	//Vector4 v1 = mvp * Vector4(1, 1, 0, 1);
-	//Vector4 v2 = mvp * Vector4(1, -1, 0, 1);
-	//Vector4 v3 = mvp * Vector4(-1, -1, 0, 1);
-	//Vector4 v4 = mvp * Vector4(-1, 1, 0, 1);
-	//Vector4 v5 = mvp * Vector4(-1, 1, 1, 1);
-	//Vector4 v6 = mvp * Vector4(-1, -1, 1, 1);
-	//Vector4 v7 = mvp * Vector4(1, -1, 1, 1);
-	//Vector4 v8 = mvp * Vector4(1, 1, 1, 1);
+	Point pos = aabb.GetCenter();
+	Size size = aabb.GetSize();
+	const Matrix4 mvp = cam.Matrix() * Matrix4::CreateLocalToWorld(pos, Euler(0, 0, 0), size);
+	
+	Vector4 v;
+	
+	Vector4 v1 = mvp * Vector4( 1,  1, 0, 1);
+	Vector4 v2 = mvp * Vector4( 1, -1, 0, 1);
+	Vector4 v3 = mvp * Vector4(-1, -1, 0, 1);
+	Vector4 v4 = mvp * Vector4(-1,  1, 0, 1);
+	Vector4 v5 = mvp * Vector4(-1,  1, 2, 1);
+	Vector4 v6 = mvp * Vector4(-1, -1, 2, 1);
+	Vector4 v7 = mvp * Vector4( 1, -1, 2, 1);
+	Vector4 v8 = mvp * Vector4( 1,  1, 2, 1);
 	
 	const Matrix4 mat = cam.Matrix();
-	
-	Vector3 v;
-	
-	Vector3 v1 = mat.MultiplyPoint(Vector3(1, 1, 0));
-	Vector3 v2 = mat.MultiplyPoint(Vector3(1, -1, 0));
-	Vector3 v3 = mat.MultiplyPoint(Vector3(-1, -1, 0));
-	Vector3 v4 = mat.MultiplyPoint(Vector3(-1, 1, 0));
-	Vector3 v5 = mat.MultiplyPoint(Vector3(-1, 1, 1));
-	Vector3 v6 = mat.MultiplyPoint(Vector3(-1, -1, 1));
-	Vector3 v7 = mat.MultiplyPoint(Vector3(1, -1, 1));
-	Vector3 v8 = mat.MultiplyPoint(Vector3(1, 1, 1));
 
 	glBegin(GL_LINE_LOOP);
 	DrawVertex(v1);
